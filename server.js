4e6b893f05afe4d2079c8b6983b5622f168b6b20
sravi4701 <sravi4701@gmail.com>
2017-02-06 00:01:45 +0530

    Change server.js

--- server.js
+++ server.js
@@ -4,13 +4,87 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles = {
+	'article-one' : {
+		title: 'Article-one | superman',
+		heading: 'Article 1 Begins',
+		date: 'Feb 5, 2017',
+		content: `
+				<p>
+	                this is content of paragraphs this is content of paragraphs this is content of paragraphs this is content of paragraphs
+	                this is content of paragraphs this is content of paragraphs this is content of paragraphs this is content of paragraphs
+	            </p>
+	            <p>
+	                this is content of paragraphs this is content of paragraphs this is content of paragraphs this is content of paragraphs
+	                this is content of paragraphs this is content of paragraphs this is content of paragraphs this is content of paragraphs
+	            </p>
+	            <p>
+	                this is content of paragraphs this is content of paragraphs this is content of paragraphs this is content of paragraphs
+	                this is content of paragraphs this is content of paragraphs this is content of paragraphs this is content of paragraphs
+	            </p>`
+	},
+	'article-two' : {
+		title: 'Article-two | batman',
+		heading: 'Article 2 Begins',
+		date: 'Feb 10, 2017',
+		content: `
+			<p>
+				Contents of article two. 
+	        </p>
+		`
+	},
+	'article-three': {
+		title: 'Article-Three | ironman',
+		heading: 'Article 3 Begins',
+		date: 'Feb 15, 2017',
+		content:`
+			<p>
+				Contents of article three. 
+	        </p>
+		`
+	}
+}
+function createTemplate(data){
+	var title = data.title;
+	var head = data.heading;
+	var content = data.content;
+	var date = data.date;
+	var html_template = `
+	<!DOCTYPE html>
+	<html>
+	    <head>
+	       <title>
+	       		${title}
+	       </title>
+	       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    </head>
+	    <body>
+	        <div>
+	            <a href="/">Home</a>
+	        </div>
+	        <hr/>
+	        <h3>
+	        	${head}
+	        </h3>
+	        <div>
+	            ${date}
+	        </div>
+	        <div>
+	        	${content}
+	        </div>
+	    </body>
+	</html>
+	`;
+	return html_template;
+}
+
+app.get('/:articleName', function(req, res){  // :articleName look for the names from req and expand it.
+	var articleName = req.params.articleName;
+   	res.send(createTemplate(articles[articleName]));
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/article-one', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
