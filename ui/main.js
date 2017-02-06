97abc46c7b637fed5f4824d7bebf2260f01ff0af
ravi shankar <sravi4701@users.noreply.github.com>
2017-02-06 23:36:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,20 @@
 button  = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-    counter = counter + 1;
-    data = document.getElementById('count');
-    data.innerHTML = counter.toString();
+    //create a request
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter  = request.responseText;
+                data = document.getElementById('count');
+                data.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET', 'http://sravi4701.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
